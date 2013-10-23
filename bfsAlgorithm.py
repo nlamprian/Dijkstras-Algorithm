203fda5bd400b3b0c9b71b62fd95cf14de7892b4
Nick Lamprianidis <lampnick67@yahoo.com>
2013-10-23 21:37:12 +0300

    Update bfsAlgorithm.py

--- bfsAlgorithm.py
+++ bfsAlgorithm.py
@@ -1,33 +1,44 @@
-''' Breadth First Search Algorithm
-	Note: works on both undirected and directed graphs
-    Input:  adj_list - a dictionary with vertices as keys,
-                       and edges as values in the form of list of neighboring vertices
-    	    s - source vertex
-    Output: a set of all reachable vertices from the source vertex s '''
-
 import sys
 
-def BFS(adj_list,s):
-	explored, frontier = {s}, [s]
+
+def BFS(adj_list, source):
+	'''
+	Breadth First Search Algorithm
+	
+	Works on both undirected and directed graphs
+
+    Args:
+    	adj_list: a dictionary where the keys are vertex ids, and the values are lists of vertex ids.
+				  each pair of key-value_j, for value_j \in adj_list[key], constitutes an edge.
+		source: the id of the vertex that acts as a starting point for the algorithm
+
+    Returns:
+    	explored: a set of ids of all reachable vertices from the source vertex
+	'''
+	explored, frontier = {source}, [source]
+
 	while frontier:
 		v = frontier.pop(0)
 		for w in adj_list[v]:
 			if w not in explored:
 				explored.add(w)
 				frontier.append(w)
+
 	return explored
 
 
+
 if __name__ == "__main__":
-	# Expects a file (adjacency list), and a source vertex
-	if len(sys.argv) < 3: sys.exit("Error: No input filename or source vertex.")
-	filename, s = sys.argv[1], int(sys.argv[2])
+	# Expects a filename (adjacency list), and an id (source vertex)
+	if len(sys.argv) < 3: sys.exit("Error: No filename or source vertex id")
+	filename, source = sys.argv[1], int(sys.argv[2])
 
 	adj_list = {}
 	with open(filename, 'r') as f:
-		for line in f:
+		# Builds the adjacency list
+		for line in f:  # v w1 w2 w3 ...
 			vertices = line.split()
 			adj_list[int(vertices[0])] = [int(v) for v in vertices[1:]]
 
-	explored = BFS(adj_list,s)
+	explored = BFS(adj_list, source)
 	print(explored)
