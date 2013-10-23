1db6a1bde615cddaa0545b6544644a5b700c88c7
Nick Lamprianidis <lampnick67@yahoo.com>
2013-10-23 21:39:26 +0300

    Update connectedComponents.py

--- connectedComponents.py
+++ connectedComponents.py
@@ -1,29 +1,39 @@
-''' Algorithm for computing the connected components of an undirected graph
-    Input:  adj_list - a dictionary with vertices as keys,
-                       and edges as values in the form of list of neighboring vertices
-    Output: cc - a list of sets, where each set represents a connected component '''
-
 import sys
 from bfsAlgorithm import BFS
 
+
 def connectedComponents(adj_list):
+	'''
+	Computes the connected components of an undirected graph
+
+	Args:
+		adj_list: a dictionary where the keys are vertex ids, and the values are lists of vertex ids.
+				  each pair of key-value_j, for value_j \in adj_list[key], constitutes an edge.
+
+	Returns:
+		cc: a list of sets, where each set holds the vertex ids of the corresponding connected component
+	'''
 	explored, cc = set(), []
-	for v in range(1,len(adj_list)+1):
+
+	for v in adj_list:
 		if v not in explored:
-			c = BFS(adj_list,v)
+			c = BFS(adj_list, v)
 			explored |= c
 			cc.append(c)
+
 	return cc
 
 
+
 if __name__ == "__main__":
-	# Expects a file (adjacency list)
-	if len(sys.argv) < 2: sys.exit("Error: No input filename.")
+	# Expects a filename (adjacency list)
+	if len(sys.argv) < 2: sys.exit("Error: No filename")
 	filename = sys.argv[1]
 
 	adj_list = {}
 	with open(filename, 'r') as f:
-		for line in f:
+		# Builds the adjacency list
+		for line in f:  # v w1 w2 w3 ...
 			vertices = line.split()
 			adj_list[int(vertices[0])] = [int(v) for v in vertices[1:]]
 
